4ddbfe67a8daa85f18554ba2c43e357aaf3f6f4c
LorTrindade <lortrindade@gmail.com>
2021-04-13 15:24:35 -0300

    refactoring from controller to restcontroller

--- hr-payroll/src/main/java/com/lortrindade/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/lortrindade/hrpayroll/controller/PaymentController.java
@@ -4,12 +4,12 @@ import com.lortrindade.hrpayroll.entities.Payment;
 import com.lortrindade.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/payments")
 public class PaymentController {
 
